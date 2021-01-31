d686243a1d62734b850469ccf85bc66830679655
jecanizarez <je.canizarez@uniandes.edu.co>
2021-01-31 11:21:22 -0500

    archivos iniciales

---
+++ API/app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
